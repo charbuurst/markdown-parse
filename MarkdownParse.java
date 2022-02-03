d10ca4b20b56aac4ca4f12faa1b638d6421cc1eb
charbuurst <chn018@ucsd.edu>
2022-02-02 16:45:31 -0800

    fixed bug
    format.md returns the link when there are spaces between the bracket and the parantheses.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -28,7 +28,11 @@ public class MarkdownParse {
             if(openParen == -1){
                 return toReturn;
             }
-
+            if (openParen - nextCloseBracket != 1)
+            {
+                currentIndex = closeParen +1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             
